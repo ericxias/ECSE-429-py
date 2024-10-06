1aaaab29b65d8f309419ed5c2033c8efab7c7ef0
ericxias <90631685+ericxias@users.noreply.github.com>
2024-10-06 18:37:57 -0400

    fixed bugs in create update, and read methods

--- src/test/TodoTest.py
+++ src/test/TodoTest.py
@@ -2,98 +2,114 @@ import pytest
 import requests
 import json
 
-""" def test_get_todos_with_id():
+def test_get_todos_with_id():
     # Retrieve all todos and test read the first todo if any todos exist
     response = requests.get('http://localhost:4567/todos')
+    todos = response.json()
 
-    if response.status_code == 200 and response.json() != []:
+    # Check if any todos exist
+    if response.status_code == 200 and isinstance(todos, dict) and len(todos) > 0:
         # Get the id of the first todo
-        todo_id = response.json()[0]["id"]
+        todo_id = todos["todos"][0]["id"]
+        print(todo_id)
         response1 = requests.get('http://localhost:4567/todos/' + str(todo_id))
-
-        assert response1.json() == response.json()[0]
+        assert response1.json() == {'todos': [todos["todos"][0]]}
         assert response1.status_code == 200
 
     else:
         # If there is no todo, test the error message when attempting to get todo with id 1
         response2 = requests.get('http://localhost:4567/todos/1')
-        assert response2.json() == [{"errorMessages":["Could not find an instance with todos/1"]}]
+        assert response2.json() == {"errorMessages":["Could not find an instance with todos/1"]}
         assert response2.status_code == 404
 
 
-def test_get_categories_with_todo_id():
+def test_get_categories_with_todos_id():
     # Retrieve all todos and test read the categories of the first todo if any todos with category relationships exist
     response = requests.get('http://localhost:4567/todos')
+    todos = response.json()
 
-    if response.status_code == 200 and response.json() != []:
+    # bug in the application -> if the id of the todo inputted is not in the database, the application will output the first category always instead of an error message
+    # Correct behaviour:
+    # Check if any todos exist
+    if response.status_code == 200 and isinstance(todos, dict) and len(todos) > 0:
         # Get the id of the first todo
-        todo_id = response.json()[0]["id"]
-        # Check if the first todo has any categories
+        todo_id = todos["todos"][0]["id"]
 
-        if response.json()[0]["categories"] != []:
+        # Check if the first todo has any categories
+        if "categories" in todos["todos"][0] and todos["todos"][0]["categories"] != []:
             # Get the id of the first category of the first todo and check that the categories of the first todo match the categories of the first category
             response1 = requests.get('http://localhost:4567/todos/' + str(todo_id) + '/categories')
             category_data = {}
-            # for each category of the first todo, get the category data and add it to the category_data dictionary
+            
+            # Check if the categories of the first todo match the category data retrieved
+            for i in range(len(todos["todos"][0]["categories"]) - 1):
+                response2 = requests.get('http://localhost:4567/categories/' + todos["todos"][0]["categories"][i+1]["id"])
+                category_data += response2.json()["categories"]
 
-            for i in range(len(response.json()[0]["categories"]) - 1):
-                response2 = requests.get('http://localhost:4567/categories/' + response.json()[0]["categories"][i+1]["id"])
-                category_data += response2.json()
-
-            # Check if the categories of the first todo match the category data retrieved        
-            response3 = requests.get('http://localhost:4567/categories/' + response.json()[0]["categories"][0]["id"])
-            assert category_data == response3.json()
+            response3 = requests.get('http://localhost:4567/todos/'+ todo_id + '/categories')
+            assert response3.json() == {"categories" : [category_data]}
             assert response1.status_code == 200
 
         else:
             # If no categories exist, return an empty list
-            response1 = requests.get('http://localhost:4567/todos/' + str(todo_id) + '/categories')
-            assert response1.json() == [{"categories":[]}]
-            assert response1.status_code == 200
+            response4 = requests.get('http://localhost:4567/todos/' + str(todo_id) + '/categories')
+            assert response4.json() == {"categories": []}
+            assert response4.status_code == 200
+    else:
+        # another bug in the application -> if no todos exist, the application will output {"categories":[]} always
+        response5 = requests.get('http://localhost:4567/todos/1/categories')
+        assert response5.json() == {"categories": []}
+        assert response5.status_code == 200
+        
 
 
-def test_get_tasksof_with_todo_id():
-    # Retrieve all todos and test read the tasksof of the first todo if any todos with tasksof relationships exist
+def test_get_tasksof_with_todos_id():
+    # Retrieve all todos and test read the first todo if any todos exist
     response = requests.get('http://localhost:4567/todos')
+    todos = response.json()
 
-    if response.status_code == 200 and response.json() != []:
+    # Check if any todos exist
+    if response.status_code == 200 and isinstance(todos, dict) and len(todos) > 0:
         # Get the id of the first todo
-        todo_id = response.json()[0]["id"]
-        # Check if the first todo has any tasksof relationships
+        todo_id = todos["todos"][0]["id"]
 
-        if response.json()[0]["tasksof"] != []:
+        # Check if the first todo has any tasksof relationships
+        if "tasksof" in todos["todos"][0] and todos["todos"][0]["tasksof"] != []:
             # Get the id of the first tasksof of the first todo and check that the tasksof of the first todo match the tasksof of the first tasksof
             response1 = requests.get('http://localhost:4567/todos/' + str(todo_id) + '/tasksof')
             tasksof_data = {}
             # for each tasksof of the first todo, get the tasksof data and add it to the tasksof_data dictionary
 
-            for i in range(len(response.json()[0]["tasksof"]) - 1):
-                response2 = requests.get('http://localhost:4567/tasksof/' + response.json()[0]["tasksof"][i+1]["id"])
-                tasksof_data += response2.json()
+            for i in range(len((todos["todos"][0]["tasksof"])) - 1):
+                response2 = requests.get('http://localhost:4567/tasksof/' + todos["todos"][0]["tasksof"][i+1]["id"])
+                tasksof_data += response2.json()["tasksof"]
 
             # Check if the tasksof of the first todo match the tasksof data retrieved        
-            response3 = requests.get('http://localhost:4567/tasksof/' + response.json()[0]["tasksof"][0]["id"])
-            assert tasksof_data == response3.json()
+            response3 = requests.get('http://localhost:4567/todos/'+ todo_id + '/tasksof')
+            assert response3.json() == {"projects": [tasksof_data]}
             assert response1.status_code == 200
 
         else:
             # If no tasksof relationships exist, return an empty list
             response1 = requests.get('http://localhost:4567/todos/' + str(todo_id) + '/tasksof')
-            assert response1.json() == [{"projects":[]}]
+            assert response1.json() == {"projects":[]}
             assert response1.status_code == 200
- """
+    else:
+        # When attempting to get tasksof of a todo that does not exist, assert error
+        response2 = requests.get('http://localhost:4567/todos/1/tasksof')
+        assert response2.status_code == 404
+        
 
 def test_post_create_todo():
     # Test create new todo
     response = requests.post('http://localhost:4567/todos', json={"title":"test","doneStatus": False,"description":""})
     assert response.status_code == 201
-    new_todo_id = response.json()["id"]
-    print(new_todo_id)
+    todo_id = response.json()["id"]
 
     # Check if the new todo is created
-    response2 = requests.get('http://localhost:4567/todos/' + str(new_todo_id))
+    response2 = requests.get('http://localhost:4567/todos/' + str(todo_id))
     
-    assert response2.json() == {"todos":[{"id":new_todo_id,"title":"test","doneStatus":"false","description":""}]}
+    assert response2.json() == {"todos":[{"id":todo_id,"title":"test","doneStatus":"false","description":""}]}
 
 
 def test_invalid_post_todo():
@@ -114,19 +130,18 @@ def test_invalid_post_todo():
 def test_update_todo_with_post():
     # Test update todos with id
     response = requests.get('http://localhost:4567/todos')
+    todos = response.json()
 
-    if response.status_code == 200:
-        todos = response.json()
+    # Check if any todos exist
+    if response.status_code == 200 and isinstance(todos, dict) and len(todos) > 0:
+        # Get the id of the first todo
+        todo_id = todos["todos"][0]["id"]
+        response1 = requests.post('http://localhost:4567/todos/' + str(todo_id), json={"title":"test","doneStatus": True,"description":""})
 
-        # Check if any todos exist
-        if isinstance(todos, list) and len(todos) > 0:
-            # Get the id of the first todo
-            todo_id = todos[0]["id"]
-            response1 = requests.post('http://localhost:4567/todos/' + str(todo_id), json={"title":"test","doneStatus": True,"description":""})
-
-            # Check if the todo is updated
-            assert response1.json() == {"id":todo_id,"title":"test","doneStatus":"true","description":"","tasksof":[{"id":"1"}]}
-            assert response1.status_code == 200
+        # Check if the todo is updated
+        response2 = requests.get('http://localhost:4567/todos/' + str(todo_id))
+        assert response1.json() == response2.json()["todos"][0]
+        assert response1.status_code == 200
 
     else:
         # If there is no todo, test the error message when attempting to update todo with id 1
@@ -139,23 +154,23 @@ def test_update_todo_with_put():
     # Test update todos with id
     response = requests.get('http://localhost:4567/todos')
 
-    if response.status_code == 200:
-        todos = response.json()
+    todos = response.json()
 
-        # Check if any todos exist
-        if isinstance(todos, list) and len(todos) > 0:
-            # Get the id of the first todo
-            todo_id = todos[0]["id"]
-            response1 = requests.put('http://localhost:4567/todos/' + str(todo_id), json={"title":"test","doneStatus": True,"description":""})
+    # Check if any todos exist
+    if response.status_code == 200 and isinstance(todos, dict) and len(todos) > 0:
+        # Get the id of the first todo
+        todo_id = todos["todos"][0]["id"]
+        response1 = requests.put('http://localhost:4567/todos/' + str(todo_id), json={"title":"test","doneStatus": True,"description":""})
 
-            # Check if the todo is updated
-            assert response1.json() == {"id":todo_id,"title":"test","doneStatus":"true","description":"","tasksof":[{"id":"1"}]}
-            assert response1.status_code == 200
+        # Check if the todo is updated
+        response2 = requests.get('http://localhost:4567/todos/' + str(todo_id))
+        assert response1.json() == response2.json()["todos"][0]
+        assert response1.status_code == 200
 
-            # test the error message when attempting to update without inputting a title
-            response2 = requests.put('http://localhost:4567/todos/' + str(todo_id), json={"doneStatus": True,"description":""})
-            assert response2.json() == {"errorMessages":["title : field is mandatory"]}
-            assert response2.status_code == 400
+        # test the error message when attempting to update without inputting a title
+        response2 = requests.put('http://localhost:4567/todos/' + str(todo_id), json={"doneStatus": True,"description":""})
+        assert response2.json() == {"errorMessages":["title : field is mandatory"]}
+        assert response2.status_code == 400
 
     else:
         # If there is no todo, test the error message when attempting to update todo with id 1
