cb233e63b661fb9b4d8bfbea1f69e16140f8a1fd
rashyaathar <98115083+rashyaathar@users.noreply.github.com>
2024-10-07 21:20:40 -0400

    Added new tests and bugfixed old tests.

--- src/test/ProjectTest.py
+++ src/test/ProjectTest.py
@@ -18,7 +18,7 @@ def test_get_projects_with_id():
         project_id = projects["projects"][0]["id"]
         print(project_id)
         response1 = requests.get('http://localhost:4567/projects/' + str(project_id))
-        assert response1.json == {'projects': [projects["projects"][0]]}
+        assert response1.json() == {'projects': [projects["projects"][0]]}
         assert response1.status_code == 200
     ## If not found, test error response.
     else:
@@ -53,19 +53,55 @@ def test_post_create_project():
     response2 = requests.get('http://localhost:4567/projects/' + str(project_id))
     assert response2.json() == {"projects":[{"id":project_id,"title":"test","completed":"false","active":"false","description":"test description"}]}
 
-def test_invalid_create_project():
+def test_invalid_post_create_project():
     # Test create new project with invalid inputs.
     response = requests.post('http://localhost:4567/projects', json={"id":1,"completed": False,"active": False,"description":"test description"})
     assert response.json() == {"errorMessages": ["Invalid Creation: Failed Validation: Not allowed to create with id"]}
     assert response.status_code == 400
 
     response2 = requests.post('http://localhost:4567/projects', json={"title":"test","completed": False,"active":"false","description":"test description"})
-    assert response2.json() == {"errorMessages":["Failed Validation: doneStatus should be BOOLEAN"]}
+    assert response2.json() == {"errorMessages":["Failed Validation: active should be BOOLEAN"]}
     assert response2.status_code == 400
 
     response3 = requests.post('http://localhost:4567/projects', json={"title":"test","completed":"false","active": False,"description":"test description"})
-    assert response3.json() == {"errorMessages":["Failed Validation: doneStatus should be BOOLEAN"]}
+    assert response3.json() == {"errorMessages":["Failed Validation: completed should be BOOLEAN"]}
     assert response3.status_code == 400
+
+def test_post_update_project():
+    # Get all projects.
+    response = requests.get('http://localhost:4567/projects')
+    projects = response.json()
+
+    if response.status_code == 200 and isinstance(projects, dict) and len(projects) > 0:
+        project_id = projects["projects"][0]["id"]
+        response1 = requests.post('http://localhost:4567/projects/' + str(project_id), json={"title":"test2","completed": False, "active": False, "description":"test2 description"})
+        assert response1.status_code == 200
+
+        response2 = requests.get('http://localhost:4567/projects/' + str(project_id))
+        assert response1.json() == response2.json()["projects"][0]
+    
+    else:
+        response3 = requests.post('http://localhost:4567/projects/' + str(project_id), json={"title":"test2","completed": False, "active": False, "description":"test2 description"})
+        assert response3.json() == {"errorMessages":["No such project entity instance with GUID or ID 1 found"]}
+        assert response3.status_code == 404
+    
+def test_put_update_project():
+    # Get all projects.
+    response = requests.get('http://localhost:4567/projects')
+    projects = response.json()
+
+    if response.status_code == 200 and isinstance(projects, dict) and len(projects) > 0:
+        project_id = projects["projects"][0]["id"]
+        response1 = requests.put('http://localhost:4567/projects/' + str(project_id), json={"title":"test2","completed": False, "active": False, "description":"test2 description"})
+        assert response1.status_code == 200
+
+        response2 = requests.get('http://localhost:4567/projects/' + str(project_id))
+        assert response1.json() == response2.json()["projects"][0]
+    
+    else:
+        response3 = requests.put('http://localhost:4567/projects/1', json={"title":"test2","completed": False, "active": False, "description":"test2 description"})
+        assert response3.json() == {"errorMessages":["Invalid GUID for 1 entity todo"]}
+        assert response3.status_code == 404
 
 def test_delete_project():
     # Test delete project with id.
