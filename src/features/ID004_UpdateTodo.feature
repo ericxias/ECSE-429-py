312830acddbb4c3b398b77a09f45e9976b808df3
ericxias <90631685+ericxias@users.noreply.github.com>
2024-10-26 19:31:05 -0400

    fixed some issues with previous IDs, finished ID004

--- src/features/ID004_UpdateTodo.feature
+++ src/features/ID004_UpdateTodo.feature
@@ -2,15 +2,18 @@ Feature: Update a Todo Task Instance
     As a user of the rest api todo list manager application
     I want to update the information of a todo task
     So I can organize the tasks I need to complete
-        
+
+    Background:
+    Given a Todo task with title "test", doneStatus "false", and description "test" exists in the system
+
     Scenario: User updates all information of a Todo task (Normal Flow)
-        When the user updates a Todo task with new title "shred paperwork" and doneStatus "True" and description "shred all the paperwork"
+        When the user updates a Todo task with new title "shred paperwork", doneStatus "True", and description "shred all the paperwork"
         Then the Todo task should have title "shred paperwork", doneStatus "true", and description "shred all the paperwork"
     
     Scenario: User updates only the title of a Todo task (Alternate Flow)
         When the user updates a Todo task with new title "shred paperwork" 
-        Then the Todo task should have title "shred paperwork"
+        Then the Todo task should have title "shred paperwork", doneStatus "false", and description "test"
     
     Scenario: User updates a Todo task without a Title (Error Flow)
         When the user updates a Todo task with new doneStatus "True" and description "shred all the paperwork"
-        Then the system should return an error message "title : field is mandatory"
+        Then the system should return an error message "title : field is mandatory" and status code "400"
--- src/features/steps/step_definitions.py
+++ src/features/steps/step_definitions.py
@@ -9,10 +9,14 @@ from behave import *
 # ID001_TodoCategoryRelationship.feature
 
 @given('a Todo task with title "{title}" and doneStatus "{doneStatus}" and a Category with title "{cTitle}" exists in the system')
-def step_given_todo_and_category_exist(context, title, doneStatus, cTitle):
+def step_impl(context, title, doneStatus, cTitle):
     done_status_bool = process_bool(doneStatus)
-    todo_response = requests.post(f'http://localhost:4567/todos', json={"title": title, "doneStatus": done_status_bool})
-    category_response = requests.post(f'http://localhost:4567/categories', json={"title": cTitle, "description": "test"})
+    todo_body = {
+        "title": title,
+        "doneStatus": done_status_bool
+    }
+    todo_response = requests.post(f'http://localhost:4567/todos', json={todo_body})
+    category_response = requests.post(f'http://localhost:4567/categories', json={"title": cTitle})
     assert todo_response.status_code == 201
     assert category_response.status_code == 201
     context.todo_id = todo_response.json()['id']
@@ -21,9 +25,13 @@ def step_given_todo_and_category_exist(context, title, doneStatus, cTitle):
     print(context.category_id)
 
 @given('a relationship between a Todo task with title "{title}" and doneStatus "{doneStatus}" and a Category with title "{cTitle}" already exists in the system')
-def step_given_relationship_exists(context, title, doneStatus, cTitle):
+def step_impl(context, title, doneStatus, cTitle):
     done_status_bool = process_bool(doneStatus)
-    todo_response = requests.post(f'http://localhost:4567/todos', json={"title": title, "doneStatus": done_status_bool})
+    todo_body = {
+        "title": title,
+        "doneStatus": done_status_bool
+    }
+    todo_response = requests.post(f'http://localhost:4567/todos', json={todo_body})
     category_response = requests.post(f'http://localhost:4567/categories', json={"title": cTitle})
     relationship_response = requests.post(f'http://localhost:4567/todos/{todo_response.json()["id"]}/categories', json={"id": category_response.json()["id"]})
     assert todo_response.status_code == 201
@@ -33,8 +41,8 @@ def step_given_relationship_exists(context, title, doneStatus, cTitle):
     context.category_id = category_response.json()['id']
 
 @given('a Category does not exist')
-def step_given_category_does_not_exist(context):
-    todo_response = requests.post(f'http://localhost:4567/todos', json={"title": "test", "doneStatus": False})
+def step_impl(context):
+    todo_response = requests.post(f'http://localhost:4567/todos', json={"title": "test"})
     category_response = requests.post('http://localhost:4567/categories', json={"title": "test"})
     assert todo_response.status_code == 201
     assert category_response.status_code == 201
@@ -45,18 +53,21 @@ def step_given_category_does_not_exist(context):
 
 @when('the user creates a relationship between the Todo and the Category')
 def step_impl(context):
-    body = {"id": context.category_id}
-    response = requests.post(f'http://localhost:4567/todos/{context.todo_id}/categories', json=body)
+    response = requests.post(f'http://localhost:4567/todos/{context.todo_id}/categories', json={"id": context.category_id})
     context.response = response
 
 @then('the relationship between Todo and Category is created successfully')
-def step_then_verify_relationship_created(context):
+def step_impl(context):
     response = context.response
     assert response.status_code == 201
 
 @then('the system should return an error message "{error_message}" and status code "{status_code}"')
-def step_then_verify_error_message(context, error_message, status_code):
+def step_impl(context, error_message, status_code):
     response = context.response
+    # print(response.status_code)
+    # print(status_code)
+    # print(response.json())
+    # print(error_message)
     assert response.status_code == int(status_code)
     assert response.json()['errorMessages'][0] == error_message
 
@@ -64,24 +75,42 @@ def step_then_verify_error_message(context, error_message, status_code):
 # ID002_TodoProjectRelationship.feature
 
 @given('a Todo task with title "{title}" and doneStatus "{doneStatus}" and a project with title "{ptitle}", completed "{completed}", and active "{active}" exists in the system')
-def step_given_todo_and_project_exist(context, title, doneStatus, ptitle, completed, active):
+def step_impl(context, title, doneStatus, ptitle, completed, active):
     done_status_bool = process_bool(doneStatus)
     completed_bool = process_bool(completed)
     active_bool = process_bool(active)
-    todo_response = requests.post(f'http://localhost:4567/todos', json={"title": title, "doneStatus": done_status_bool})
-    project_response = requests.post(f'http://localhost:4567/projects', json={"title": ptitle, "completed": completed_bool, "active": active_bool})
+    todo_body = {
+        "title": title,
+        "doneStatus": done_status_bool
+    }
+    project_body = {
+        "title": ptitle,
+        "completed": completed_bool,
+        "active": active_bool
+    }
+    todo_response = requests.post(f'http://localhost:4567/todos', json={todo_body})
+    project_response = requests.post(f'http://localhost:4567/projects', json={project_body})
     assert todo_response.status_code == 201
     assert project_response.status_code == 201
     context.todo_id = todo_response.json()['id']
     context.project_id = project_response.json()['id']
 
 @given('a relationship between a Todo task with title "{title}" and doneStatus "{doneStatus}" and a project with title "{ptitle}", completed "{completed}", and active "{active}" already exists in the system')
-def step_given_relationship_exists(context, title, doneStatus, ptitle, completed, active):
+def step_impl(context, title, doneStatus, ptitle, completed, active):
     done_status_bool = process_bool(doneStatus)
     completed_bool = process_bool(completed)
     active_bool = process_bool(active)
-    todo_response = requests.post(f'http://localhost:4567/todos', json={"title": title, "doneStatus": done_status_bool})
-    project_response = requests.post(f'http://localhost:4567/projects', json={"title": ptitle, "completed": completed_bool, "active": active_bool})
+    todo_body = {
+        "title": title,
+        "doneStatus": done_status_bool
+    }
+    project_body = {
+        "title": ptitle,
+        "completed": completed_bool,
+        "active": active_bool
+    }
+    todo_response = requests.post(f'http://localhost:4567/todos', json={todo_body})
+    project_response = requests.post(f'http://localhost:4567/projects', json={project_body})
     relationship_response = requests.post(f'http://localhost:4567/todos/{todo_response.json()["id"]}/tasksof', json={"id": project_response.json()["id"]})
     assert todo_response.status_code == 201
     assert project_response.status_code == 201
@@ -89,9 +118,9 @@ def step_given_relationship_exists(context, title, doneStatus, ptitle, completed
     context.todo_id = todo_response.json()['id']
     context.project_id = project_response.json()['id']
 
-@given('a project does not exist')
-def step_given_project_does_not_exist(context):
-    todo_response = requests.post(f'http://localhost:4567/todos', json={"title": "test", "doneStatus": False})
+@given('a Project does not exist')
+def step_impl(context):
+    todo_response = requests.post(f'http://localhost:4567/todos', json={"title": "test"})
     project_response = requests.post('http://localhost:4567/projects', json={"title": "test"})
     assert todo_response.status_code == 201
     assert project_response.status_code == 201
@@ -102,33 +131,42 @@ def step_given_project_does_not_exist(context):
 
 @when('the user creates a relationship between the Todo and the Project')
 def step_impl(context):
-    body = {"id": context.project_id}
+    body = {
+        "id": context.project_id
+        }
     response = requests.post(f'http://localhost:4567/todos/{context.todo_id}/tasksof', json=body)
     context.response = response
 
 @then('the relationship between Todo and Project is created successfully')
-def step_then_verify_relationship_created(context):
+def step_impl(context):
     response = context.response
     assert response.status_code == 201
 
 # ID003_CreateTodo.feature
 
 @when('the user creates a Todo task with title "{title}" and doneStatus "{doneStatus}" and description "{description}"')
-def step_when_create_todo_task(context, title, doneStatus, description):
+def step_impl(context, title, doneStatus, description):
     done_status_bool = process_bool(doneStatus)
-    body = {"title": title, "doneStatus": done_status_bool, "description": description}
+    body = {
+        "title": title, 
+        "doneStatus": done_status_bool, 
+        "description": description
+        }
     response = requests.post('http://localhost:4567/todos', json=body)
     context.response = response
 
 @when('the user creates a Todo task with title "{title}" and doneStatus "{doneStatus}"')
-def step_when_create_todo_task(context, title, doneStatus):
+def step_impl(context, title, doneStatus):
     done_status_bool = process_bool(doneStatus)
-    body = {"title": title, "doneStatus": done_status_bool}
+    body = {
+        "title": title, 
+        "doneStatus": done_status_bool
+        }
     response = requests.post('http://localhost:4567/todos', json=body)
     context.response = response
 
 @then('the Todo task is successfully created with title "{title}", doneStatus "{doneStatus}", and description "{description}"')
-def step_then_verify_todo_created(context, title, doneStatus, description):
+def step_impl(context, title, doneStatus, description):
     response = context.response
     assert response.status_code == 201
     assert response.json()['title'] == title
@@ -136,7 +174,7 @@ def step_then_verify_todo_created(context, title, doneStatus, description):
     assert response.json()['description'] == description
 
 @then('the Todo task is successfully created with title "{title}", doneStatus "{doneStatus}", and no description')
-def step_then_verify_todo_created(context, title, doneStatus):
+def step_impl(context, title, doneStatus):
     response = context.response
     assert response.status_code == 201
     assert response.json()['title'] == title
@@ -145,9 +183,64 @@ def step_then_verify_todo_created(context, title, doneStatus):
 
 # ID004_UpdateTodo.feature
 
+@given('a Todo task with title "{title}", doneStatus "{doneStatus}", and description "{description}" exists in the system')
+def step_impl(context, title, doneStatus, description):
+    done_status_bool = process_bool(doneStatus)
+    body = {
+        "title": title,
+        "doneStatus": done_status_bool,
+        "description": description
+    }
+    response = requests.post('http://localhost:4567/todos', json=body)
+    assert response.status_code == 201
+    context.todo_id = response.json()['id']
 
+@when('the user updates a Todo task with new title "{title}", doneStatus "{doneStatus}", and description "{description}"')
+def step_impl(context, title, doneStatus, description):
+    done_status_bool = process_bool(doneStatus)
+    body = {
+        "title": title, 
+        "doneStatus": done_status_bool, 
+        "description": description
+        }
+    response = requests.post(f'http://localhost:4567/todos/{context.todo_id}', json=body)
+    context.response = response    
+
+@when('the user updates a Todo task with new title "{title}"')
+def step_impl(context, title):
+    response = requests.post(f'http://localhost:4567/todos/{context.todo_id}', json={"title": title})
+    context.response = response
+
+@when('the user updates a Todo task with new doneStatus "{doneStatus}" and description "{description}"')
+def step_impl(context, doneStatus, description):
+    done_status_bool = process_bool(doneStatus)
+    body = {
+        "doneStatus": done_status_bool, 
+        "description": description
+        }
+    response = requests.put(f'http://localhost:4567/todos/{context.todo_id}', json=body)
+    context.response = response
+
+@then('the Todo task should have title "{title}", doneStatus "{doneStatus}", and description "{description}"')
+def step_impl(context, title, doneStatus, description):
+    response = context.response
+    assert response.status_code == 200
+    assert response.json()['title'] == title
+    assert response.json()['doneStatus'] == doneStatus
+    assert response.json()['description'] == description
 
 # ID005_DeleteTodo.feature
+
+
+
+
+
+
+
+
+
+
+
 
 # processing strings to boolean if the string is "true" or "false"
 def process_bool(string):
